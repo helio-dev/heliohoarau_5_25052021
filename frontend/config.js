ddfa70aa867e694485719717a4588d8b588fdade
helio.dev <helio.dev@protonmail.com>
2021-05-25 12:28:54 +0200

    config.js : gestion des erreur

--- frontend/config.js
+++ frontend/config.js
@@ -1,18 +1,23 @@
 main()
-function main(){
-    const articles = getTeddiesArticle()
+async function main(){
+    const articles = await getTeddiesArticle()
+    console.log(articles)
     displayArticles(articles)
 }
-
-function getTeddiesArticle(){
+//faire une fonction async permet d'utiliser await et donc return fetch//
+ function getTeddiesArticle(){
     //fetch va chercher les articles //
-    fetch("http://localhost:3000/api/teddies")
+     return fetch("http://localhost:3000/api/teddies")
     // fonction, a executer quand les données auront étés récupérées //
         .then(function(httpBodyResponse){ //Un return dans un then se recupère dans le then suivant //
           return httpBodyResponse.json()
         })
         .then(function(articles){
-            console.log(articles)
+            return articles
+        })
+        //Backup de gestion d'erreur au cas ou l'api soit down et que le fecth n'arrive pas à destination//
+        .catch(function(error){
+            alert(eror)
         })
 }
 
