64701603ca6bbbd080e8ed8244c7bd99a4b90d4f
helio.dev <helio.dev@protonmail.com>
2021-05-26 15:45:29 +0200

    restructuration du js

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,23 +1,15 @@
 fetch ("http://localhost:3000/api/teddies")
-	.then ((res)=>res.json()
-	.then ((resultat)=>{
+.then ((res)=>res.json()
+.then((resultat)=>{
+	let tableauElements = [];
+	for (let cur of resultat)
+	{
+		tableauElements.push(document.createElement("div"));
+		document.body.appendChild(tableauElements[tableauElements.length-1]);
 
-let tableauElements = [];
-for (let cur of resultat) {
-
-
-	tableauElements.push(document.createElement("div"));
-	document.body.appendChild(tableauElements[tableauElements.length-1]);
-
-	tableauElements.push(document.createElement("p"));
-	tableauElements[tableauElements.length-1].onclick = () => console.log (hello);
-	tableauElements[tableauElements.length-1].textContent= cur.name;
-	tableauElements[tableauElements.length-2].appendChild(tableauElements[tableauElements.length-1]);
-
-	tableauElements.push(document.createElement("img"));
-  tableauElements[tableauElements.length-3].src=cur.imageUrl;
-
-}
-
-
+		tableauElements.push(document.createElement("p"));
+		tableauElements[tableauElements.length-1].onclick = () => console.log ("hello");
+		tableauElements[tableauElements.length-1].textContent = cur.name;
+		tableauElements[tableauElements.length-2].appendChild(tableauElements[tableauElements.length-1]);
+	}
 }))
